44e1d8639bda7c2fe5bc7c545d776066c8256609
Lucas7yoshi <Lucas7yoshi@gmail.com>
2022-06-04 00:45:51 -0400

    Optimize and add better native documentation

--- code/components/extra-natives-five/src/ModelListNatives.cpp
+++ code/components/extra-natives-five/src/ModelListNatives.cpp
@@ -16,36 +16,27 @@ static uint32_t(*initVehicleArchetype)(const char* name, bool a2, unsigned int a
 
 static void(*g_origArchetypeDtor)(fwArchetype* at);
 
-static std::unordered_map<uint32_t, std::string> g_vehiclesHashes;
-static std::vector<std::string> g_vehicles;
+// Use a map of hashes and names for optimization purposes
+// We just hash it on registration instead of needing to rehash it when checking for it every deregistration
+static std::unordered_map<uint32_t, std::string> g_vehicles;
 
 static uint32_t initVehicleArchetype_stub(const char* name, bool a2, unsigned int a3) 
 {	
-	// check if name already exists in g_vehicles, if so print a warning
-	if (g_vehicles.end() != std::find(g_vehicles.begin(), g_vehicles.end(), name))
+	uint32_t hash = HashString(name);
+	if (g_vehicles.find(hash) == g_vehicles.end())
 	{
-		//trace("[VehicleArchetype] Warning: Vehicle archetype %s already exists!\n", name);
-		console::PrintWarning("extra-natives-five", "Vehicle archetype %s already was registered!\n", name);
-	}
-	else
-	{
-		g_vehicles.emplace_back(name);
-		//console::Printf("extra-natives-five", "Added vehicle archetype %s\n", name);
+		g_vehicles.insert({hash, name});
 	}
 	return initVehicleArchetype(name, a2, a3);
 }
 
 static void ArchetypeDtorHook(fwArchetype* at)
 {
-	//at->assetType is probably relevant here, but I don't know what it is for vehicles
-	auto hash = HashString("foobar");
-	for (auto& veh : g_vehicles)
+	if (at->assetType == 1)
 	{
-		if (HashString(veh.c_str()) == at->hash)
+		if (g_vehicles.find(at->hash) != g_vehicles.end())
 		{
-			g_vehicles.erase(std::remove(g_vehicles.begin(), g_vehicles.end(), veh), g_vehicles.end());
-			console::Printf("extra-natives-five", "Removed vehicle %s from g_vehicles\n", veh.c_str());
-			break;
+			g_vehicles.erase(at->hash);
 		}
 	}
 	g_origArchetypeDtor(at);
@@ -55,13 +46,11 @@ static HookFunction hookFunction([]()
 {
 	//hook for vehicle registering
 	{
-		console::Printf("extra-natives-five", "hooking vehicle thing\n");
 		auto location = hook::get_pattern<char>("E8 ? ? ? ? 48 8B 4D E0 48 8B 11");
 		hook::set_call(&initVehicleArchetype, location);
 		hook::call(location, initVehicleArchetype_stub);
-		console::PrintWarning("extra-natives-five", fmt::sprintf("Hook location is %s\n", location));
 	}
-	// hook to catch registration of vehicles
+	// hook to catch deregistration of vehicles
 	{
 		auto location = hook::get_pattern("E8 ? ? ? ? 80 7B 60 01 74 39");
 		hook::set_call(&g_origArchetypeDtor, location);
@@ -70,8 +59,13 @@ static HookFunction hookFunction([]()
 	{
 		fx::ScriptEngine::RegisterNativeHandler("GET_ALL_VEHICLE_MODELS", [](fx::ScriptContext& context)
 		{
-			console::PrintWarning("extra-natives-five", "Native is running??\n");
-			context.SetResult(fx::SerializeObject(g_vehicles));
+			// its redundant and possibly confusing to give both hashes and names, so just give the names.
+			std::vector<std::string> models;
+			for (auto& pair : g_vehicles)
+			{
+				models.push_back(pair.second);
+			}
+			context.SetResult(fx::SerializeObject(models));
 		});
 	}
 });
